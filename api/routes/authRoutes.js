97984bd632fefdf199b460bb2695d0d7c03875dc
Joan Aquino <joan.aquino.vasquez@gmail.com>
2026-03-02 22:34:19 -0500

    fix: eliminar carpeta api duplicada y organizar raíz

--- api/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-// Ruta para el login con Google
-// POST /api/auth/google
-router.post('/google', authController.googleLogin);
-
-module.exports = router;
--- api/routes/partnerRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const partnerController = require('../controllers/partnerController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.post('/', protect, partnerController.createPartner);
-router.get('/', partnerController.getPartners);
-
-module.exports = router;
